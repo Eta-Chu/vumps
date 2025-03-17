5f59b0e9b837969746512ed9ed233f8a014d2dd8
eta_chu <eta.chu0@gmail.com>
2025-03-17 22:25:52 +0800

    test module

--- test/test_model.py
+++ test/test_model.py
@@ -2,12 +2,15 @@ import sys
 import os
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../'))
 sys.path.append(project_root)
-from vumps import UniformMPS
+print(project_root)
+from vumps import UniformMPS, SpinHalfXXZChain
 
 
 if __name__ == '__main__':
     D = 30
     d = 2
     mps = UniformMPS.random(D, d)
-    mps.leftCanonical()
-    print(mps.canonical_form)
+    mps.mixCanonical()
+    model = SpinHalfXXZChain(delta=1)
+    e = model.energy(mps)
+    print(e)
